1970e35a667762ff9ee27db4f920c2aceaf09a08
Rishabh <rishabh26402@gmail.com>
2025-02-15 18:40:58 +0530

    completed react basics

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,18 @@
+import Chai from "./Chai";
+
 
 function App() {
 
+  const userName = "chai aur code"
+
   return (
-    <h1>React Basics | with Rishabh</h1>
+    //   this <></> is called fragment
+    <>
+    <Chai />
+    <h2>Rishabh {userName}</h2>
+    {/* {} this is a expression, and {userName} it is evaluated expression, means whatever evaluation we have to done should be done under a variable, and only the variable name will be given inside the expression which holds the return value  */}
+    <p>This is react class</p>
+    </>
   )
 }
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// function MyApp(){
+//   return(
+//     <div>
+//       <h1>Custom App ! function calling </h1>
+//     </div>
+//   )
+// }
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
+
+const anotherUser = " Rishabh is on field work"
+const reactElement = React.createElement( // syntax to create element within variable
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'Click me to visit google',
+  anotherUser  // evaluated expression 
+)
+
+// const AnotherElement = ( //  same variable as ReactElement container but with correct syntax
+//   <a href="hhttps://google.com" target='_blank'>Visit Google</a>
+// )
+
+
+ReactDOM.createRoot(document.getElementById('root')).
+render(
+  // reactElement
+  // <App />
+
 )
