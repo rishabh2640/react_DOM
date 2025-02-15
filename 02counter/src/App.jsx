484073b08ea643a399abfc29c75833fcb09a5b72
Rishabh <rishabh26402@gmail.com>
2025-02-15 23:07:55 +0530

    counter project completed

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0)
+
+  // let counter = 0;
+
+  const addValue = function(){
+    if (counter < 20) {
+      
+      counter = counter + 1; // counter value in HTML page won't get update automatically because that will be only handled by React
+      console.log(counter);   
+      setCounter(counter) // so this way, using useState you can update UI that is HTML page
+    }
+  }
+
+  const decreases = () => {
+    if (counter > 0) {
+      console.log(counter - 1)
+      counter = counter - 1;
+      setCounter(counter);
+    }
+  }
+  const reset = () => {
+    
+    counter = 0;
+    console.log(counter)
+      setCounter(counter);
+  }
+
+  return (
+    <>
+    <h1>Rishabh aur Reach</h1>
+    <h2>Counter Value: {counter}</h2>
+
+    <button
+    onClick={addValue} 
+    // not calling the addValue because we don't want to execute addValue, since we want addValue should be execute when button us clicked we are passing addValue as a reference without ()
+    >Add Value</button>
+    <br />
+    <br />
+    <button
+    onClick={decreases}
+    >Decrease Value</button>
+    <br />
+    <br />
+    <button
+    onClick={reset}
+    >Reset Counter</button>
+    </>
+  )
+}
+
+export default App
