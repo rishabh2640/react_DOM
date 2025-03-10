e1a8fa967f25d10e461de47b7e94fc38a2b83b0c
Rishabh <rishabh26402@gmail.com>
2025-03-10 13:54:54 +0530

    context api project

---
+++ 08miniContext/src/context/UserContext.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const UserContext = React.createContext() // this is to create Context , and then we have to use provider to provide data
+
+export default UserContext;
