bde6a0043838ada35a9da886de7ba5be154a5c2d
Rishabh <rishabh26402@gmail.com>
2025-03-22 11:39:06 +0530

    context

--- 09themeSwitcher/src/App.jsx
+++ 09themeSwitcher/src/App.jsx
@@ -2,11 +2,17 @@ import './App.css'
 
 function App() {
 
-
-  return (
-    <>
-
-    </>
+  return ( 
+      <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  {/*themeBtn*/}
+              </div>
+              <div className="w-full max-w-sm mx-auto">
+                  {/*Card*/}
+              </div>
+          </div>
+      </div>
   )
 }
 
