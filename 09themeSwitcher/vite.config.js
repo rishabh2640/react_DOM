05e8dcffb47f196c9235d2917ff322fe662017b2
Rishabh <rishabh26402@gmail.com>
2025-03-28 16:04:36 +0530

    09 theme changer finalised completed

--- 09themeSwitcher/vite.config.js
+++ 09themeSwitcher/vite.config.js
@@ -4,12 +4,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  darkMode: "class",
-  theme: {
-    extend: {},
-  },
   plugins: [react(), tailwindcss(),],
 })
+
 
 
 
