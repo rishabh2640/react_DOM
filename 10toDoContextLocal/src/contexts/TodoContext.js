f0bd691021590c3de97fe6fabc3b48a33e858eec
Rishabh <rishabh26402@gmail.com>
2025-04-05 10:48:20 +0530

    project 10 edit completion

--- 10toDoContextLocal/src/contexts/TodoContext.js
+++ 10toDoContextLocal/src/contexts/TodoContext.js
@@ -1,13 +1,7 @@
 import { createContext, useContext } from "react";
 
 export const TodoContext = createContext({
-    todos: [
-        {
-            id: 1,
-            todo: "Todo msg",
-            completed: false,
-        },
-    ],
+    todos: [{}],
     addTodo: (todo) => { },
     updateTodo: (id, todo) => { },
     deleteTodo: (id) => { },
