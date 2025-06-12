d14a14b33ba7796cc9f363d37a91bd2df1d89840
Rishabh <rishabh26402@gmail.com>
2025-06-12 23:50:19 +0530

    Completed the project, but there are some bugs to be solved

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -4,6 +4,14 @@ import Container from "./Container/Container"
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn"
 import Input from './Input'
+import Button from "./Button"
+import RTE from "./RTE"
+import Select from './Select'
+import Signup from './Signup'
+import Login from './Login'
+import PostForm from "./post-form/PostForm"
+import PostCard from './PostCard'
+import AuthLayout from './AuthLayout'
 
 export {
     Header,
@@ -11,5 +19,13 @@ export {
     Container,
     Logo,
     LogoutBtn,
-    Input
+    Input,
+    Button,
+    RTE,
+    Select,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout
 }
